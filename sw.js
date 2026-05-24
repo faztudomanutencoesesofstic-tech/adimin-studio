d6a47032bde908a9b1ff7f8a4a82ff07366adb5c
faztudomanutencoesesofstic-tech <faztudomanutencoesesofstic@gmail.com>
2026-05-24 09:03:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = 'studio-cache-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  // Mantém a conexão direta com o banco de dados Firebase
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
