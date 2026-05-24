4d818b6fe992cc1b59fbfa03584637d86d188a4f
faztudomanutencoesesofstic-tech <faztudomanutencoesesofstic@gmail.com>
2026-05-24 11:11:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-const CACHE_NAME = 'studio-cache-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'studio-admin-cache-v2';
+const assets = ['./', './index.html', './manifest.json', './logo.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets).catch(err => console.log("Aviso de cache inicial:", err));
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
