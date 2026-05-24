46d2953f1735dfa458d5d103876f9022a6bb8bcf
faztudomanutencoesesofstic-tech <faztudomanutencoesesofstic@gmail.com>
2026-05-24 00:08:13 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Mantém a conexão direta com o banco de dados Firebase
+});
